155850fc45e6eaceb8ee6f2d1d715db969c4d900
LINJOUOM ALAIN PASCAL <linjouom9@gmail.com>
2025-10-16 06:36:44 +0100

    ajout de flask-cors

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "http://localhost:3000"], "methods": ["GET", "POST"], "allow_headers": ["Content-Type"]}})
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bd.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
